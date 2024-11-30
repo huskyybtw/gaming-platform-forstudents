dde3ac025258e55d4f4bb211065c9de0264067b9
huskybtw <marekkocikw@gmail.com>
2024-11-30 02:23:46 +0100

    Interceptor Logic

--- backend/src/main/java/pwr/isa/backend/Security/WebMvcConfig.java
+++ backend/src/main/java/pwr/isa/backend/Security/WebMvcConfig.java
@@ -8,18 +8,22 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebMvcConfig implements WebMvcConfigurer {
 
     private final UserCustomInterceptor userCustomInterceptor;
-    private final AdminCustomInterceptor adminCustomInterceptor;
+    private final UserSpecificCustomInterceptor userSpecificCustomInterceptor;
 
-    public WebMvcConfig(UserCustomInterceptor userCustomInterceptor,AdminCustomInterceptor adminCustomInterceptor) {
+    public WebMvcConfig(UserCustomInterceptor userCustomInterceptor, UserSpecificCustomInterceptor userSpecificCustomInterceptor) {
         this.userCustomInterceptor = userCustomInterceptor;
-        this.adminCustomInterceptor = adminCustomInterceptor;
+        this.userSpecificCustomInterceptor = userSpecificCustomInterceptor;
     }
-
+    // every GET method is ignored
+    // if token is assigned to admin, it has access to all endpoints
+    // userInterceptor ensures valid token is provided for all endpoints
+    // userSpecificInterceptor ensures that requests related to specific user has valid token for that user
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(userCustomInterceptor)
-                .addPathPatterns("/api/v1/user/**");
-        registry.addInterceptor(adminCustomInterceptor)
+                .addPathPatterns("/api/v1/user/**")
+                .excludePathPatterns("/api/v1/users");
+        registry.addInterceptor(userSpecificCustomInterceptor)
                 .addPathPatterns("/api/v1/admin/**");
     }
 }
