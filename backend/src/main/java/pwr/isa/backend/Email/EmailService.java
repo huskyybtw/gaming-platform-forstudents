0cfa5842bd9c18d2becaf90fa46a3c2923588914
huskybtw <marekkocikw@gmail.com>
2024-12-07 18:09:52 +0100

    Working html in email-sender

--- backend/src/main/java/pwr/isa/backend/Email/EmailService.java
+++ backend/src/main/java/pwr/isa/backend/Email/EmailService.java
@@ -1,6 +1,6 @@
 package pwr.isa.backend.Email;
 
 public interface EmailService {
-    void sendEmail(String userEmail,Long userId);
+    void sendEmail(String userEmail,Long userId) throws  Exception;
     void confirmEmail(String confirm);
 }
