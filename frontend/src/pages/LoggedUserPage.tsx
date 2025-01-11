fe00c5b1df6eb5edd0e3f29d003da330e1bcb644
Michał Pawlik <pawlikm11@gmail.com>
2025-01-11 14:44:46 +0100

    działa wstępnie MatchPoster, TeamPoster, User Poster, lepszy interface

--- frontend/src/pages/LoggedUserPage.tsx
+++ frontend/src/pages/LoggedUserPage.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import './MainPage.css';
+import '../styles/MainPage.css';
 
 interface MatchPoster {
     id: number;
--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
-import './LoginPage.css';
+import '../styles/LoginPage.css';
 
 interface LoginResponse {
     token: string;
