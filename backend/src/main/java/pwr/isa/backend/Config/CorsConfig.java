315d6e95566918faac547b3bc2be2f2db49e536d
huskybtw <marekkocikw@gmail.com>
2025-01-11 16:09:22 +0100

    cors + port consistency resolved

--- backend/src/main/java/pwr/isa/backend/Config/CorsConfig.java
+++ backend/src/main/java/pwr/isa/backend/Config/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
-                        .allowedOrigins("http://localhost:5174")
+                        .allowedOrigins("http://localhost:*")
                         .allowedHeaders("*")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowCredentials(true); // If cookies or credentials are used
